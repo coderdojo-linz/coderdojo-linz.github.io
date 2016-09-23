7717aebe574d65c910d098313c7befa256b6444b
Rainer Stropek <rainer@software-architects.at>
2016-09-23 14:04:45 +0200

    .

--- trainingsanleitungen/fundamentals/create-ubuntu-scripts/install-dev-tools.sh
+++ trainingsanleitungen/fundamentals/create-ubuntu-scripts/install-dev-tools.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install -qqy build-essential gdb
+sudo apt-get install -qqy build-essential gdb libc6-dev-i386
 curl --output nasm-2.12.02.tar.gz http://www.nasm.us/pub/nasm/releasebuilds/2.12.02/nasm-2.12.02.tar.gz
 sudo tar -xzf nasm-2.12.02.tar.gz --directory /usr/local/src
 cd /usr/local/src/nasm-2.12.02/
