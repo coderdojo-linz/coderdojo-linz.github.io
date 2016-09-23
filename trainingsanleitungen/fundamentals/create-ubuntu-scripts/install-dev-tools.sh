a457abddef1ece9cfe1b6092e03b5dcfd2b60a53
Rainer Stropek <rainer@software-architects.at>
2016-09-23 11:20:32 +0200

    .

---
+++ trainingsanleitungen/fundamentals/create-ubuntu-scripts/install-dev-tools.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt-get install -qqy build-essential gdb
+curl --output nasm-2.12.02.tar.gz http://www.nasm.us/pub/nasm/releasebuilds/2.12.02/nasm-2.12.02.tar.gz
+sudo tar -xzf nasm-2.12.02.tar.gz --directory /usr/local/src
+cd /usr/local/src/nasm-2.12.02/
+sudo ./configure
+sudo make
+sudo make install
+sudo cp nasm.1 /usr/local/man/man1
+sudo cp ndisasm.1 /usr/local/man/man1
