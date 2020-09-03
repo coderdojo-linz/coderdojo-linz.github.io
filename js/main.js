a66dfd114f32bd8b85e5376a16f09ebe95840a18
Rainer <rainer@software-architects.at>
2020-09-03 12:22:07 +0000

    Update website

--- js/main.js
+++ js/main.js
@@ -23,27 +23,37 @@ function scrollDown() {
 function sendContactForm(type) {
     $('#contact-success').hide();
     $('#contact-error').hide();
+    console.log('sendContactForm');
 
-    var form = document.getElementById('contact-form');
-    form.classList.add('was-validated');
-    if (form.checkValidity() === true) {
+    grecaptcha.ready(function () {
+        console.log('sendContactForm 1');
+        grecaptcha.execute('6LfXNMcZAAAAALYnf3GpUqw5uotVMyjhLNU438i9', { action: 'submit' }).then(function (token) {
+            console.log('token', token);
+            var form = document.getElementById('contact-form');
+            form.classList.add('was-validated');
+            if (form.checkValidity() === true) {
 
-        $.ajax({
-            type: 'POST',
-            url: 'https://prod-168.westeurope.logic.azure.com/workflows/2dd6d3e5e6704ed1afa801d769e4708b/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=r26oUrw9V_-PToMEcu1HhY7w7-LQxyvK5H0M9U8knLc',
-            contentType: 'application/json; charset=utf-8',
-            dataType: 'json',
-            data: JSON.stringify({ email: $('#contact-email').val(), text: type + '\n\n' + $('#contact-text').val() })
-        }).done(function () {
-            $('#contact-success').show();
-        }).fail(function (error) {
-            if (error.status === 202) {
-                $('#contact-success').show();
-            } else {
-                $('#contact-error').show();
+                $.ajax({
+                    type: 'POST',
+                    url: 'https://prod-168.westeurope.logic.azure.com/workflows/2dd6d3e5e6704ed1afa801d769e4708b/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=r26oUrw9V_-PToMEcu1HhY7w7-LQxyvK5H0M9U8knLc',
+                    contentType: 'application/json; charset=utf-8',
+                    dataType: 'json',
+                    data: JSON.stringify({ 
+                        email: $('#contact-email').val(), 
+                        text: type + '\n\n' + $('#contact-text').val(),
+                        token: token })
+                }).done(function () {
+                    $('#contact-success').show();
+                }).fail(function (error) {
+                    if (error.status === 202) {
+                        $('#contact-success').show();
+                    } else {
+                        $('#contact-error').show();
+                    }
+                });
             }
         });
-    }
+    });
 }
 
 function selectCategory(category) {
