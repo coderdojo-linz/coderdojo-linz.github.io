9e01ae50a7b4bc6f02f0974e7bb9c4e3417fb5a5
Rainer <rainer@software-architects.at>
2022-10-27 12:04:37 +0000

    Update website

--- js/main.js
+++ js/main.js
@@ -263,7 +263,7 @@ function loadEvents(eventsTable) {
 
           let registrationLink = '';
           let location = '';
-          if (formattedDate === '21.10.2022') {
+          if (formattedDate === '04.11.2022') {
             if (event.location.indexOf('Steyr') >= 0) {
               registrationLink =
                 'https://tic-steyr.at/coderdojo-steyr/registration';
