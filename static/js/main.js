c039d2ae256d2203db22a5a021645a95cf5419e6
Karin Huber <karin@software-architects.at>
2023-01-16 16:12:30 +0100

    Anmeldung 27.01.

--- static/js/main.js
+++ static/js/main.js
@@ -263,7 +263,7 @@ function loadEvents(eventsTable) {
 
           let registrationLink = '';
           let location = '';
-          if (formattedDate === '02.12.2022') {
+          if (formattedDate === '27.01.2023') {
             if (event.location.indexOf('Steyr') >= 0) {
               registrationLink =
                 'https://tic-steyr.at/coderdojo-steyr/registration';
