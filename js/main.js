7592bba9625273c812b6e2fce3300b29d3831054
Rainer <rainer@software-architects.at>
2025-04-06 08:07:01 +0000

    Update website

--- js/main.js
+++ js/main.js
@@ -195,7 +195,7 @@ function loadEvents(eventsTable) {
           formattedBeginTime = '16:00';
           formattedEndTime = '18:00';
 
-          if (formattedDate === '24.01.2025') {
+          if (formattedDate === '06.06.2025') {
             if (event.location.indexOf('Leonding') >= 0) {
               formattedBeginTime = '15:00';
               formattedEndTime = '17:00';
